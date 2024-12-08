7cde8fed77d78a377a0c01db7555934dad88db76
ASUS <quocanh21095@gmail.com>
2024-12-08 16:01:26 +0700

    fix revenue again 5

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -1,7 +1,5 @@
-import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/items/table';
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { inter } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
